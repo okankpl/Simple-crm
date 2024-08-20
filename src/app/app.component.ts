cacb97f672694f856cf0bca72d35f0de7556c1e1
Okan Kaplan <okan149@gmail.com>
2024-08-20 19:58:01 +0200

    adding menu icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatSidenavModule],
+  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatSidenavModule,MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
