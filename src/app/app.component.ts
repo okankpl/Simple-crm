9b1b1724175abea92244ab7df336fac495f6d869
Okan Kaplan <okan149@gmail.com>
2024-08-20 19:51:24 +0200

    adding sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatToolbarModule],
+  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
