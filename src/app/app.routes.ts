2e3dd88ac303c99e6b52c15a983673faaf86af42
Okan Kaplan <okan149@gmail.com>
2024-08-22 16:22:56 +0200

    adding routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: DashboardComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'user', component: UserComponent },
+    
+];
