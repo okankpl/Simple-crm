cd4f0b5d9f10a7480ce5911a00e1578918284a20
Okan Kaplan <okan149@gmail.com>
2024-08-25 18:24:56 +0200

    adding add user btn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatToolbarModule,MatSidenavModule,MatIconModule,RouterModule],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule, RouterModule, MatButtonModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,MatButtonModule, MatDividerModule, MatIconModule,MatTooltipModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
