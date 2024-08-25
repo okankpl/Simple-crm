b2ef8d9631afca8e59482cfc916144a0a8d824e8
Okan Kaplan <okan149@gmail.com>
2024-08-25 19:13:03 +0200

    adding opendialog func

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTooltipModule} from '@angular/material/tooltip';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterModule,MatButtonModule, MatDividerModule, MatIconModule,MatTooltipModule],
+  imports: [RouterModule, MatButtonModule, MatDividerModule, MatIconModule, MatTooltipModule, MatDialogModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
 
+  constructor(public dialog: MatDialog) {
+    
+}
+openDialog(): void {
+  this.dialog.open(DialogAddUserComponent);
+}
 }
