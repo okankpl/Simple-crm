e34a5ac9be5fef58b2c392abfe3a188fbcd870f9
Okan Kaplan <okan149@gmail.com>
2024-08-26 21:47:23 +0200

    adding input field

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {MatDialogModule} from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogModule,MatButtonModule],
+  imports: [MatDialogModule,MatButtonModule,MatInputModule,MatFormFieldModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
