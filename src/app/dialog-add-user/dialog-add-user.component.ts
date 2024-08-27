888e946f249acd646da753b96a0c5855f0e2ba5d
Okan Kaplan <okan149@gmail.com>
2024-08-27 19:11:37 +0200

    getting birth date in time

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -7,20 +7,31 @@ import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatNativeDateModule, provideNativeDateAdapter} from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
+import { UserComponent } from '../user/user.component';
+
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatDialogModule,MatButtonModule,FormsModule,MatInputModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule],
+  imports: [MatDialogModule,UserComponent,MatButtonModule,FormsModule,MatInputModule,MatFormFieldModule,MatDatepickerModule,MatNativeDateModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
 
   user = new User();
+  birthDate: Date;
+
+  constructor() {
+    this.birthDate = new Date(); 
+  }
 
   onNoClick() {
 
+  }
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('current user is', this.user);
   }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,7 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
-
+import { User } from '../../models/user.class';
 
 
 @Component({
@@ -17,8 +17,9 @@ import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.compo
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
-
+  
+  user = new User();
+  
   constructor(public dialog: MatDialog) {
     
 }
