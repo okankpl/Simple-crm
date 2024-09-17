b9333a8741fd9cf232a1d16edb1dc8dc72a5ad30
Okan Kaplan <okan149@gmail.com>
2024-09-17 19:24:37 +0200

    adding collection data to users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -8,26 +8,26 @@ import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 import { MatCardModule } from '@angular/material/card';
-import { Firestore, collection, valueChanges } from '@angular/fire/firestore';
-
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterModule, MatButtonModule, MatDividerModule, MatIconModule, MatTooltipModule, MatDialogModule, MatCardModule],
+  imports: [RouterModule,CommonModule, MatButtonModule, MatDividerModule, MatIconModule, MatTooltipModule, MatDialogModule, MatCardModule],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-  
+  user = new User();
   users: User[] = [];
   
   constructor(public dialog: MatDialog, @Inject(Firestore) private firestore: Firestore) {}
 
   ngOnInit(): void {
     const usersCollection = collection(this.firestore, 'users');
-    valueChanges(usersCollection).subscribe((changes: User[]) => {
+    collectionData(usersCollection, { idField: 'id' }).subscribe((changes) => {
       console.log('Received changes from DB', changes);
-      this.users = changes; // Update the users array with the latest data
+      this.users = changes as User[]; 
     });
   }
 
