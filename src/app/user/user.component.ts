89f31f13821ab748e8e1cef5a31de8dd9a341a13
Okan Kaplan <okan149@gmail.com>
2024-09-17 19:06:57 +0200

    adding valuechanges and sub

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
@@ -7,27 +7,34 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
+import { Firestore, collection, valueChanges } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [RouterModule, MatButtonModule, MatDividerModule, MatIconModule, MatTooltipModule, MatDialogModule,MatCardModule],
+  imports: [RouterModule, MatButtonModule, MatDividerModule, MatIconModule, MatTooltipModule, MatDialogModule, MatCardModule],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrls: ['./user.component.scss']
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   
-  user = new User();
+  users: User[] = [];
   
-  constructor(public dialog: MatDialog) {
-    
-}
-openDialog() {
-  const dialogRef = this.dialog.open(DialogAddUserComponent);
+  constructor(public dialog: MatDialog, @Inject(Firestore) private firestore: Firestore) {}
 
-  dialogRef.afterClosed().subscribe(result => {
-    console.log('Dialog result: ', result);
-  });
-}
+  ngOnInit(): void {
+    const usersCollection = collection(this.firestore, 'users');
+    valueChanges(usersCollection).subscribe((changes: User[]) => {
+      console.log('Received changes from DB', changes);
+      this.users = changes; // Update the users array with the latest data
+    });
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogAddUserComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('Dialog result: ', result);
+    });
+  }
 }
