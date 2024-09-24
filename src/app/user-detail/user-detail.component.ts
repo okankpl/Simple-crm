81d005757654e6fc0af41f49da09b786ca8944a0
Okan Kaplan <okan149@gmail.com>
2024-09-24 18:35:27 +0200

    adding user detail card and adding dynamic information to each user

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { Firestore, doc, docData } from '@angular/fire/firestore';
+import { User } from '../../models/user.class';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrls: ['./user-detail.component.scss']
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
 
+  userId: string = '';
+  user: User = new User();
+
+  constructor(
+    private route: ActivatedRoute,
+    @Inject(Firestore) private firestore: Firestore
+  ) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(paramMap => {
+      const id = paramMap.get('id');
+      if (id !== null) {
+        this.userId = id;
+        console.log('Die Benutzer-ID ist:', this.userId);
+
+        const userDocRef = doc(this.firestore, 'users', this.userId);
+        docData(userDocRef).subscribe(userData => {
+          this.user = new User(userData);
+          console.log('Benutzerdaten sind:', this.user);
+        });
+      } else {
+        console.error('Keine Benutzer-ID in den Routenparametern gefunden.');
+      }
+    });
+  }
 }
